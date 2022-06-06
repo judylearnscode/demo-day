f7c2e8e52d1ef636d11416f48ee590ccbef81a89
Judy Chong <judyjchong@gmail.com>
2022-06-05 23:51:14 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://judychong:sp00kymuld3r@cluster0.dnbqu.mongodb.net/demoDay?retryWrites=true&w=majority', 
+
+    'dbName': 'demoDay'
+};
